e03b1bce688dd4a90744ed5b671c60af5f3fe9a5
Issac Ortega <technologyintelligenceik@gmail.com>
2020-12-29 21:58:57 -0600

    Versionando el archivo exit.js por problemas de cache

--- views/index.view.php
+++ views/index.view.php
@@ -88,7 +88,7 @@
     <script src="<?php echo RUTA ?>resources/js/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="<?php echo RUTA ?>resources/js/bootstrap.min.js"></script>
-    <script src="<?php echo RUTA ?>resources/js/exit.js"></script>
+    <script src="<?php echo RUTA ?>resources/js/exit.js?v=1"></script>
     <script>
         $(".custom-file-input").on("change", function() {
             var fileName = $(this).val().split("\\").pop();
