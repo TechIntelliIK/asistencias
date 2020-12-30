5787a33b96d8438dea6e80c7b14d694fa01b681f
Issac Ortega <technologyintelligenceik@gmail.com>
2020-12-29 19:53:48 -0600

    Agregando accesos de la DB de produccion al archivo config.php

--- data/config.php
+++ data/config.php
@@ -7,16 +7,16 @@ setlocale(LC_ALL, "es_ES");
 error_reporting(0);
 
 // Variable definida para utilizar en los enlaces o en la reedirección
-define('RUTA', "http://{$_SERVER['SERVER_NAME']}/asistencias/");
+define('RUTA', "http://{$_SERVER['SERVER_NAME']}/");
 
 //  Arreglo de configuracion de la Base de datos
 $bd_config = array(
     // Nombre de dominio o host
-    'host' => '127.0.0.1:33065',
+    'host' => 'localhost',
     // nombre de la base de datos
     'basededatos' => 'hammex_asistencias',
     // Usurio de base de datos
-    'usuario' => 'root',
+    'usuario' => 'gortega',
     // Contraseña de base de datos
-    'pass' => ''
+    'pass' => 'Issac1997'
 );
